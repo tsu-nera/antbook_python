57d7c430e58a3bfbb46eb0df378bc3e6f1efabcd
tsu-nera <fox10225fox@gmail.com>
2016-01-06 15:34:12 +0900

    ページ数でリネーム

--- chp2/p34_bubunwa.py
+++ chp2/p34_bubunwa.py
@@ -6,11 +6,15 @@ k = 13
 # a = [1, 2, 4, 7]
 # k = 15
 
-def dfs(i, acc):
-    if i == n: return acc == k
 
-    if dfs(i + 1, acc): return True
-    if dfs(i + 1, acc + a[i]): return True
+def dfs(i, acc):
+    if i == n:
+        return acc == k
+
+    if dfs(i + 1, acc):
+        return True
+    if dfs(i + 1, acc + a[i]):
+        return True
 
     return False
 
